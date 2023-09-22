aa8052efd9dc587681f511f2b340330a388e1441
Ahmedhail <aboazooz.hail@gmail.com>
2023-09-22 23:09:28 +0300

    add files

---
+++ FAQ.js
@@ -0,0 +1,15 @@
+let qu=document.querySelectorAll(".q ")
+let qh1=document.querySelectorAll(".q h1 ")
+
+
+
+qu.forEach((q)=>{
+    q.addEventListener("click",()=>{
+        q.classList.toggle("active")
+    })
+  
+})
+
+
+
+
